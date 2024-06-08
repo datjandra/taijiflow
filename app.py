e486702d77103d6a7431c8d7a447cf1650d25eb5
datjandra <donny.tjandra@gmail.com>
2024-06-08 11:20:13 -1000

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ import streamlit as st
 
 def main():
     st.title("TaijiFlow")
+
+    with st.form("user_input_form"):
+        conditions = st.text_input("Conditions")
+
+        submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        st.video("https://youtu.be/q8yZSfAOonI", start_time=24, end_time=42)
