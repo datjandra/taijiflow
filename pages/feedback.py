05c196074513a1dc72afc2b2cf35c4708cabf7ac
datjandra <donny.tjandra@gmail.com>
2024-07-04 13:45:44 -1000

    Update feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -11,30 +11,28 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
 def detect_pose():
-    if st.button("Start", key='start_button'):
-        use_webcam = True
-    else:
-        use_webcam = False
+    video_file = st.file_uploader('Upload Video')
+    if video_file is not None:
+        tmpfile = tempfile.NamedTemporaryFile(delete=False)
+        tmpfile.write(video_file.read())
 
-    if use_webcam:
-        canvas = st.image([])
-        cap = cv.VideoCapture(0)
+        cap = cv.VideoCapture(tmpfile.name)
+        canvas = st.empty()
 
         start_time = time.time()
         while cap.isOpened():
             ret, img = cap.read()
             if not ret:
                 break
-
+    
             try:
                 img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
                 pose_results = pose.process(img_rgb)
                 mp_drawing.draw_landmarks(img, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-                cv.imshow('Output', img)
                 canvas.image(img_rgb)
             except:
                 break
-            
+                
             elapsed_time = time.time() - start_time
             if elapsed_time >= 30:
                 break
