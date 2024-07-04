1fc330babb97fcc5a3f07aa6c5442bab5b1f3ebf
datjandra <donny.tjandra@gmail.com>
2024-07-04 12:02:46 -1000

    Update feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np 
 import streamlit as st
 import mediapipe as mp
+import time
 
 ## initialize pose estimator
 mp_drawing = mp.solutions.drawing_utils
@@ -9,7 +10,26 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
 def detect_pose():
-    pass
+    if st.button("Start", key='start_button'):
+        use_webcam = True
+    else:
+        use_webcam = False
+
+    if use_webcam:
+        cap = cv2.VideoCapture(0)
+
+        start_time = time.time()
+        while cap.isOpened():
+            ret, img = cap.read()
+            if not ret:
+                break
+
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= 30:
+                break
+    
+    cap.release()
+    cv2.destroyAllWindows()
 
 def main():
     st.title("Posture Feedback")
