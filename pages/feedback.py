d9bb55e423c820ba7a3034e4e6b01490829b9b35
datjandra <donny.tjandra@gmail.com>
2024-07-04 11:56:54 -1000

    Update feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -3,6 +3,11 @@ import numpy as np
 import streamlit as st
 import mediapipe as mp
 
+## initialize pose estimator
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
 def detect_pose():
     pass
 
