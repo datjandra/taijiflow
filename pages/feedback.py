4a4469c3d6ec5ad67bf7752cad1eaa4fd7f137cf
datjandra <donny.tjandra@gmail.com>
2024-07-04 11:55:32 -1000

    Update feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -1,20 +1,10 @@
 import cv2 as cv 
 import numpy as np 
 import streamlit as st
-#import matplotlib.pyplot as plt
-#from pil import Image,ImageEnhance
+import mediapipe as mp
 
 def detect_pose():
-    BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
-
-    POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+    pass
 
 def main():
     st.title("Posture Feedback")
