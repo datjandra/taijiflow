c411a12581ed36138d0a9683e6c32a99cdd4c52e
datjandra <donny.tjandra@gmail.com>
2024-07-04 11:00:23 -1000

    Create feedback.py

---
+++ pages/feedback.py
@@ -0,0 +1,27 @@
+import cv2 as cv 
+import numpy as np 
+import streamlit as st
+import matplotlib.pyplot as plt
+from pil import Image,ImageEnhance
+
+def detect_pose():
+    BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+
+    POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
+    net = cv.dnn.readNetFromTensorflow("graph_opt.pb")
+
+def main():
+    st.title("Posture Feedback")
+    st.write()
+    detect_pose()
+
+if __name__=='__main__':
+    main()
