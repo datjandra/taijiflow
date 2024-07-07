7a8c190c21356e2612cde45d5238a937cd9c4150
datjandra <donny.tjandra@gmail.com>
2024-07-07 10:26:59 -1000

    Update analysis.py

--- pages/analysis.py
+++ pages/analysis.py
@@ -6,7 +6,7 @@ POSE_URL = os.getenv('POSE_URL')
 
 def main():
     st.set_page_config(page_title="Posture Analysis")
-    components.iframe(POSE_URL)
+    components.iframe(POSE_URL, height=700)
       
 if __name__ == "__main__":
     main()
