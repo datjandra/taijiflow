d7b7185fbfba587de50a5e16d05544722617c2f7
datjandra <donny.tjandra@gmail.com>
2024-07-17 15:03:18 -1000

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,16 @@ import os
 import requests
 import streamlit as st
 from itertools import cycle
-
-from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
-from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-from clarifai_grpc.grpc.api.status import status_code_pb2
 from functools import lru_cache
+import google.generativeai as genai
 
-USER_ID = 'openai'
-APP_ID = 'chat-completion'
-# Change these to whatever model and text URL you want to use
-MODEL_ID = os.environ.get('MODEL_ID')
-MODEL_VERSION_ID = os.environ.get('MODEL_VERSION_ID')
-PAT = os.environ.get('PAT')
+GEM_MODEL = os.getenv('GEM_MODEL')
+GEM_EXERCISE_PROMPT = os.getenv('GEM_EXERCISE_PROMPT')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 SEARCH_OPTIONS = [option.strip() for option in os.environ.get('SEARCH_OPTIONS').split(",") if option.strip()]
 
-channel = ClarifaiChannel.get_grpc_channel()
-stub = service_pb2_grpc.V2Stub(channel)
-metadata = (('authorization', 'Key ' + PAT),)
-userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel(GEN_MODEL)
 
 TL_KEY = os.getenv('TL_KEY')
 TL_INDEX = os.getenv('TL_INDEX')
@@ -33,28 +25,14 @@ TL_API_HEADERS = {
 
 @lru_cache(maxsize=128)
 def condition_to_exercise(condition):
-    prompt = f"{PROMPT} {condition}"
-    post_model_outputs_response = stub.PostModelOutputs(
-        service_pb2.PostModelOutputsRequest(
-            user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
-            model_id=MODEL_ID,
-            version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
-            inputs=[
-                resources_pb2.Input(
-                    data=resources_pb2.Data(
-                        text=resources_pb2.Text(
-                            raw=prompt
-                        )
-                    )
-                )
-            ]
-        ),
-        metadata=metadata
-    )
+    prompt = f"{GEM_EXERCISE_PROMPT} Query: {condition}"
 
-    output = post_model_outputs_response.outputs[0]
-    return output.data.text.raw
-
+    try:
+        response = model.generate_content(prompt)
+        return response.text
+    except:
+        return "Unable to suggest a relevant exercise. Please rewrite query or try again later."
+    
 @lru_cache(maxsize=128)
 def source_url(video_id):
     url = f"https://api.twelvelabs.io/v1.2/indexes/{TL_INDEX}/videos/{video_id}"    
