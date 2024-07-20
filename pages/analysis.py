69ebff21112f254ce6cf98ea3d0ce4e779a86293
datjandra <donny.tjandra@gmail.com>
2024-07-20 12:15:55 -1000

    Update analysis.py

--- pages/analysis.py
+++ pages/analysis.py
@@ -22,9 +22,9 @@ userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 def main():
     st.set_page_config(page_title="Posture Analysis")
     st.title("Standing Posture Analysis")
-    st.sidebar.page_link("app.py", label="Home")
-    st.sidebar.page_link("pages/advisor.py", label="Healthy Aging Advisor")
-    st.sidebar.page_link("pages/analysis.py", label="Posture Analysis")
+    st.sidebar.page_link("app.py", label="🏠 Home")
+    st.sidebar.page_link("pages/advisor.py", label="👵 Healthy Aging Advisor")
+    st.sidebar.page_link("pages/analysis.py", label="🖼️ Posture Analysis")
 
     uploaded_file = st.file_uploader("Please upload a picture of your standing posture", type=["png", "jpg", "jpeg"])
     if uploaded_file is not None:
