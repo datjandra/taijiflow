133eaac5b2ac21afb6bdb130afc894e79197af71
datjandra <donny.tjandra@gmail.com>
2024-07-22 13:12:52 -1000

    Update breathing.py

--- pages/breathing.py
+++ pages/breathing.py
@@ -2,7 +2,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def main():
-    st.title("Simple Breathing Exercise")
+    html_title = """ 
+    <h1 style ="color:black; text-align:center;">Simple Breathing Exercise</h1>
+    """
+    st.markdown(html_title, unsafe_allow_html = True) 
     components.iframe("https://datjandra.github.io/taijiflow/breathing.html", height=500)
 
 if __name__ == "__main__":
