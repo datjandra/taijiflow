d6aec95b4e6aa358019162b702e64352c0fc56e0
datjandra <donny.tjandra@gmail.com>
2024-07-28 15:51:09 -1000

    Update analysis.py

--- pages/analysis.py
+++ pages/analysis.py
@@ -25,7 +25,6 @@ def main():
     st.title("Standing Posture Analysis")
     menu()
 
-    # st.markdown("Please upload a picture of your [zhan zhuang](https://en.wikipedia.org/wiki/Zhan_zhuang) standing posture.")
     uploaded_file = st.file_uploader("Please upload a picture of your [zhan zhuang](https://en.wikipedia.org/wiki/Zhan_zhuang) standing posture.", type=["png", "jpg", "jpeg"])
     if uploaded_file is not None:
       image = Image.open(uploaded_file)
